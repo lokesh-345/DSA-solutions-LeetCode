45c8b63acf5d5c284175a2d0f979e5ebdadc854a
lokesh <lokeshpalagiri2004@gmail.com>
2026-06-11 11:25:45 +0530

    added two sum solution

---
+++ READ.md
@@ -0,0 +1,6 @@
+# DSA-Solutions_LeetCode
+
+This Repo contains my DSA practice solutions
+
+# Topics
+  -TwoPointers
---
+++ TwoPointers/Twosum-2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int>twoSum(vector<int>&nums,int target)
+{
+    int n=nums.size();
+    int left=0,right=n-1;
+    while(left<right)
+    {
+        int sum=nums[left]+nums[right];
+        if(target == sum)
+        return {left+1,right+1};
+        else if(sum<target)
+        left++;
+        else
+        right--;
+    }
+    return {};
+}
+
+int main()
+{
+    vector<int>nums={2,7,11,15};
+    int target=9;
+    vector<int>result = twoSum(nums,target);
+    if(result.size()==2)
+    cout<<result[0]<<","<<result[1];
+    else
+    cout<<"not found";
+    return 0;
+}
